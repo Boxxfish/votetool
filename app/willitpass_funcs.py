51f2d81b6211acaa8842e11af4ea42c1ae77f0c7
Boxfish <metakirby123@gmail.com>
2019-09-22 05:20:50 -0400

    Transfer model functions

---
+++ app/willitpass_funcs.py
@@ -0,0 +1,52 @@
+from keras.models import model_from_json
+from tensorflow import keras
+from tensorflow.python.keras import backend as k
+import tensorflow as tf  
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
+import json
+from tensorflow.python.keras.backend import set_session
+from tensorflow.python.keras.models import load_model
+# graph = tf.get_default_graph()
+
+graph = tf.compat.v1.get_default_graph()
+sess = tf.get_default_session()
+
+def model_setup():
+
+	# Model reconstruction from JSON file
+	with open('models/model_architecture_1.json', 'r') as f:
+	    model = tf.keras.models.model_from_json(f.read())
+
+	# Load weights into the new model
+	model.load_weights('models/model_weights_1.h5')
+
+	model.compile(optimizer='adam',
+	              loss='binary_crossentropy',  
+	              metrics=['accuracy'])
+
+	# Load word2id trained
+	with open('models/word2id.json', 'r') as fp:
+	    word2id = json.load(fp)
+
+	return model, word2id
+
+def predict_sentiment(text, model, word2id, max_words=1000):
+    text = str(text)
+    text = text.split(" ") 
+    text = [word.lower() for word in text]
+    text = [word2id[word] for word in text] 
+    text = sequence.pad_sequences([text], maxlen=max_words)
+    # tf_config = some_custom_config
+    # sess = tf.Session(config=tf_config)
+    # graph = tf.get_default_graph()
+    # with session.graph.as_default():
+    #     k.backend.set_session(session)
+    #     with graph.as_default():
+    global sess 
+    text_pred = None
+    global graph
+    with graph.as_default():
+    	text_pred = model.predict_proba(text)
+    return text_pred
